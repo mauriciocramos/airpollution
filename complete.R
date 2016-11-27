46cf7652a8e0f6035c0283f19f66b13a70b845ca
Maurício Collaça Ramos <mauriciocramos@gmail.com>
2016-11-27 21:11:05 -0200

    finished pollutantmean.R, complete.R

--- complete.R
+++ complete.R
@@ -1,1 +1,8 @@
-#
+complete <- function(directory, id=1:332) {
+    nobs <- NULL
+    for(i in id) {
+        monitors <- read.csv(file.path(directory,sprintf('%03d.csv',i)))
+        nobs <- c(nobs,length(monitors[complete.cases(monitors),'ID']))
+    }
+    data.frame(id,nobs)
+}
--- pollutantmean.R
+++ pollutantmean.R
@@ -1,1 +1,8 @@
-#
+pollutantmean <- function(directory, pollutant, id=1:332) {
+    pollutant_data<-NULL
+    for(i in id) {
+        monitors<-read.csv(file.path(directory,sprintf('%03d.csv',i)))
+        pollutant_data<-c(pollutant_data,monitors[!is.na(monitors[,pollutant]),pollutant])
+    }
+    mean(pollutant_data)
+}
