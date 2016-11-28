05e47308de4c8e72c6912bcd2154d19c05e5d10c
Maurício Collaça Ramos <mauriciocramos@gmail.com>
2016-11-28 01:30:05 -0200

    test

--- complete.R
+++ complete.R
@@ -1,8 +1,32 @@
-complete <- function(directory, id=1:332) {
+#
+#Part 2
+#
+#Write a function that reads a directory full of files and reports the number of
+#completely observed cases in each data file. The function should return a data
+#frame where the first column is the name of the file and the second column is
+#the number of complete cases.
+
+#' Title
+#'
+#' @param directory 
+#' @param id 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+complete <- function(directory, id = 1:332) {
     nobs <- NULL
     for(i in id) {
-        monitors <- read.csv(file.path(directory,sprintf('%03d.csv',i)))
-        nobs <- c(nobs,length(monitors[complete.cases(monitors),'ID']))
+        monitors <- read.csv(file.path(directory,sprintf('%03d.csv', i)))
+        # data.frame
+        
+        good_obs <- complete.cases(monitors)
+        # logical vector
+        
+        nobs <- c(nobs,length(monitors[good_obs,'ID']))
+        # integer vector
     }
     data.frame(id,nobs)
+    # data.frame
 }
--- pollutantmean.R
+++ pollutantmean.R
@@ -1,8 +1,35 @@
-pollutantmean <- function(directory, pollutant, id=1:332) {
-    pollutant_data<-NULL
+# 
+# Part 1
+# 
+# Write a function named 'pollutantmean' that calculates the mean of a pollutant
+# (sulfate or nitrate) across a specified list of monitors. The function
+# 'pollutantmean' takes three arguments: 'directory', 'pollutant', and 'id'.
+# Given a vector monitor ID numbers, 'pollutantmean' reads that monitors'
+# particulate matter data from the directory specified in the 'directory'
+# argument and returns the mean of the pollutant across all of the monitors,
+# ignoring any missing values coded as NA.
+
+#' Title
+#'
+#' @param directory 
+#' @param pollutant 
+#' @param id 
+#'
+#' @return
+#' @export
+#'
+#' @examples
+pollutantmean <- function(directory, pollutant, id = 1:332) {
+    pollutant_data <- NULL
     for(i in id) {
-        monitors<-read.csv(file.path(directory,sprintf('%03d.csv',i)))
-        pollutant_data<-c(pollutant_data,monitors[!is.na(monitors[,pollutant]),pollutant])
+        monitors <- read.csv(file.path(directory, sprintf('%03d.csv', i)))
+        # data.frame
+        
+        good_obs <- !is.na(monitors[ , pollutant])
+        # logical vector
+        
+        pollutant_data <- c(pollutant_data,monitors[good_obs,pollutant])
+        # numerical vector
     }
     mean(pollutant_data)
 }
